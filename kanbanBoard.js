cd97df1e33158c3427d1aab12be04aa222e111f7
Alexis <alexisr1993@gmail.com>
2020-01-25 00:30:19 -0800

    Fixed New Note and Delete

--- kanbanBoard.js
+++ kanbanBoard.js
@@ -2,8 +2,19 @@ class KanbanNote extends React.Component {
 	constructor(props){
 		super(props);
 		this.state = {
-			kanban_note_text: "This is just some sample text for this note"
+			kanban_note_text: "This is just some sample text for this note",
+			kanban_note_id: this.props.id
 		}
+		this.handleEdit = this.handleEdit.bind(this);
+		this.handleDelete = this.handleDelete.bind(this);
+	}
+
+	handleEdit() {
+
+	}
+
+	handleDelete() {
+		this.props.onDelete(this.state.kanban_note_id);
 	}
   render() {
     return (
@@ -36,8 +47,8 @@ class KanbanNote extends React.Component {
 						<div className="row">
 							<div className="col">
 								<div className="nav justify-content-end">
-									<a href="#" className="btn btn-outline-info">E</a>
-									<a href="#" className="btn btn-outline-danger">D</a>
+									<a href="#" className="btn btn-outline-info" >E</a>
+									<a href="#" className="btn btn-outline-danger" onClick={this.handleDelete}>D</a>
 								</div>
 							</div>
 						</div>
@@ -52,29 +63,41 @@ class KanbanColumn extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			notes_in_column: []
+			notes_in_column: [],
 		}
 		this.handleClick = this.handleClick.bind(this);
+		this.handleDelete = this.handleDelete.bind(this);
 	}
 
-	handleClick(){
-		let new_note = <KanbanNote key={this.state.notes_in_column.length + 1}></KanbanNote>
-
+	handleClick() {
+		let new_note_key = Date.now() + this.state.notes_in_column.length + 1;
+		// You probably shouldn't build the element here.
+		let new_note_object = { element: <KanbanNote key={new_note_key} id={new_note_key} onDelete={this.handleDelete}></KanbanNote>,
+														key: new_note_key
+													}
 		this.setState({
-			notes_in_column: [...this.state.notes_in_column, new_note]
+			notes_in_column: [...this.state.notes_in_column, new_note_object]
+		});
+	}
+
+	handleDelete(id) {
+		console.log(id);
+		this.setState({
+			notes_in_column: this.state.notes_in_column.filter(note => note.key != id)
 		});
 	}
 
 	render() {
+		let notes = this.state.notes_in_column.map(note => note.element);
   	return (
     	<div className="col-md-4">
-			<h5 className="text-center">Column Title</h5>
-				{this.state.notes_in_column}
-					<div className="row">
-						<div className="col" id="kanban-column-options">
-							<a href="#" className="btn btn-outline-primary" onClick={this.handleClick}>New Note</a>
-						</div>
+				<h5 className="text-center">{this.props.title}</h5>
+				{notes}
+				<div className="row" id="kanban-column-options">
+					<div className="col text-center">
+						<a href="#" className="btn btn-outline-primary" onClick={this.handleClick}>New Note</a>
 					</div>
+				</div>
       </div>
     );
   }
@@ -85,9 +108,9 @@ class KanbanBoard extends React.Component {
   	return (
     	<div className="container">
 				<div className="row">
-					<KanbanColumn></KanbanColumn>
-					<KanbanColumn></KanbanColumn>
-					<KanbanColumn></KanbanColumn>
+					<KanbanColumn key="left" title="Todo"></KanbanColumn>
+					<KanbanColumn key="center" title="Pending"></KanbanColumn>
+					<KanbanColumn key="right" title="Done"></KanbanColumn>
 				</div>
       </div>
     );
